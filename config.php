546f9996a2ed2a4bc08dc78e830f0cd5d785f8dd
Hansetech <info@hansetech.in>
2024-12-07 16:39:14 +0530

    commit file L1
    1st commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'pgsql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'upgrade41';
+$CFG->dbuser    = 'postgres';
+$CFG->dbpass    = '1234';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://localhost/upgrade41';
+$CFG->dataroot  = 'C:\\xampp\\upgrade41data';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
---
+++ learning_report.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>Dashboard - Learning Data</title>
+<!-- Tell the browser to be responsive to screen width -->
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+</head>
+<body class="skin-blue sidebar-mini">
+    <iframe title="Training_Dashboard" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=2fb1ad52-a8e4-4c72-a0b5-51b5a654f92d&autoAuth=true&ctid=c6a0bb68-86fd-45b9-86de-6b67903fc932" frameborder="0" allowFullScreen="true"></iframe>
+</body>
+</html>
+
